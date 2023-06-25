23bbd2df29862b1af47c6260220d4663640519b8
Nir Goldman <nirg@moonsite.co.il>
2023-06-25 16:56:28 +0300

    Add colours definition

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     fontFamily: {
       roboto: ['Roboto'],
     },
-    extend: {},
+    extend: {
+        colors: {
+          'atera-pink': '#FF176B',
+          'gold': '#D1AD78',
+          'smoky-black': '#1A1A1A',
+          'smoky-white': '#EDEDED',
+          'atera-dark-gray': '#898989',
+          'atera-light-gray': '#a9a9a9'
+        },
+    },
   },
   plugins: [],
 }
