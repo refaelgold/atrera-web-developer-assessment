8e72e72391b8fd9f477d44a44673a3ec8922fccb
Nir Goldman <nirg@moonsite.co.il>
2023-06-26 08:19:13 +0300

    Add External JS file instead inline Javascript + Modify function caller to document ready

---
+++ src/js/typewriter.js
@@ -0,0 +1,15 @@
+'use strict';
+
+document.addEventListener("DOMContentLoaded", function() {
+    let typewriter = document.getElementById("typewriter");
+    typeWriter(typewriter);
+});
+
+function typeWriter(el) {
+    const textArray = el.innerHTML.split('');
+    el.innerHTML = '';
+    textArray.forEach((letter, i) =>
+        setTimeout(() => (el.innerHTML += letter), 95 * i)
+    );
+}
+
